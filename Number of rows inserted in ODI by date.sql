5cdca2b843a5c58148893ec924ba6181aadaaf36
Borna <58553321+diskosuperstar@users.noreply.github.com>
2021-09-24 12:26:49 +0200

    Update Number of rows inserted in ODI by date.sql

--- Number of rows inserted in ODI by date.sql
+++ Number of rows inserted in ODI by date.sql
@@ -14,3 +14,12 @@ from wc_odi_run_stats_f
 where trunc(start_time) between trunc(sysdate - 1) and trunc(sysdate - 0)
 and load_plan_name IN (/*'PROJECT FUSION FREQUENT REFRESH_23_20200327_113249',*/'PROJECT FUSION DAILY REFRESH_22_20190315_073423')
 order by load_time_total desc;
+
+--top 10 of above
+select * from(
+select target_table, scenario_name,start_time, load_time, rows_inserted,rows_updated
+from wc_odi_run_stats_f
+where trunc(start_time) IN '23-SEP-21'
+and load_plan_name IN (/*'PROJECT FUSION FREQUENT REFRESH_23_20200327_113249',*/'PROJECT FUSION DAILY REFRESH_22_20190315_073423')
+order by load_time_total desc)
+where ROWNUM <= 10;
