463ee5061fd0877121bf4b9f432129fbe9bf3d66
Borna <58553321+diskosuperstar@users.noreply.github.com>
2021-10-22 14:59:35 +0200

    Update python connection to oracle db.py

--- python connection to oracle db.py
+++ python connection to oracle db.py
@@ -2,3 +2,6 @@ import cx_Oracle
 dsn = cx_Oracle.makedsn('hostname', 'port', service_name='servicename')
 orcl = cx_Oracle.connect(user='db user', password='db pass', dsn=dsn)
 cursor = orcl.cursor()
+rs = cursor.execute("select * from SNP_LP_INST")
+for row in rs:
+    print(row)
