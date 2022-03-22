bb1488207f1045550420fbf022377eca4523f923
Borna <58553321+diskosuperstar@users.noreply.github.com>
2022-03-22 18:23:38 +0100

    Update source tables from ODI.sql

--- source tables from ODI.sql
+++ source tables from ODI.sql
@@ -1,3 +1,27 @@
+SELECT
+    sp.pop_name       odi_interface_name,
+    sst.table_name    source_table_name --this is source table name!!!
+FROM
+    snp_pop          sp,
+    snp_pop_col      spc,
+    snp_col          sc,
+    snp_pop_mapping  spm,
+    snp_txt_crossr   stc,
+    snp_col          sc2,
+    snp_source_tab   sst
+WHERE
+        sp.i_pop = spc.i_pop
+    AND spc.i_col = sc.i_col
+    AND spc.i_pop_col = spm.i_pop_col
+    AND spm.i_txt_map = stc.i_txt
+    AND stc.i_col = sc2.i_col
+    AND sc2.i_table = sst.i_table
+    AND sp.table_name LIKE '%WC_UNIFIER_DRCT_FLD_COST_F'
+    group by sp.pop_name, sst.table_name;
+    
+ /
+ --Source : https://community.oracle.com/tech/apps-infra/discussion/4292253/how-to-find-source-datwarehouse-model-target-tables-in-odi-11g
+
 select
 
 SP.I_POP,
